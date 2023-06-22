7213b581d7b6bddd2473d0f2d5f5e8edad01b346
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-06-22 07:43:50 +0300

    farr

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,19 +1,23 @@
 import {useSelector} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+//import { useState,useEffect } from 'react';
 
-export default loginPage()
+
+ const LoginPage = ()=>
 {
-    const {username,password,validPwd,validUsername} = useSelector((store)=>store.login);
+
+    const navigate = useNavigate();
+    const {userData,validPwd,validUsername} = useSelector((store)=>store.login);
 
     
-    return(
-        {username?
+    return(<>{userData?
         <div style={{width:'90vw',height:'80vh'}}>
             <div className=" w3-card-4 w3-round-large w3-white w3-display-middle" style={{padding:'20px',width:'30vw'}}>
-                <h1 style={{fontWeight:'500'}} className="w3-text-blue">Welcome, {username}</h1>
-                <button className="w3-btn w3-blue w3-card-4 w3-round-large" onClick={() => {setUser(null)}}>log out</button>
+                <h1 style={{fontWeight:'500'}} className="w3-text-blue">Welcome, {userData.name}.</h1>
+                <button className="w3-btn w3-blue w3-card-4 w3-round-large" >log out</button>
             </div>
         </div>
-        :    
+          :
         <div style={{width:'90vw',height:'80vh'}}>
             <div className=" w3-card-4 w3-round-large w3-white w3-display-middle" style={{padding:'20px',width:'30vw'}}>
                 <h1 style={{fontWeight:'500'}} className="w3-text-blue">Login</h1>
@@ -24,9 +28,11 @@ export default loginPage()
                     <label htmlFor="id">Password</label><br />
                     <input type="password" id="password"  className="w3-round"/>{validPwd?'':<span className="w3-red">Invalid username</span>}
                     <br /><br />
-                    <button className="w3-btn w3-blue w3-card-4 w3-round-large" onClick={login} style={{marginRight:"2vw"}}>Log in</button> <button className="w3-btn w3-blue w3-card-4 w3-round-large" onClick={() => navigate('Register')}>Register</button>
+                    <button className="w3-btn w3-blue w3-card-4 w3-round-large"  style={{marginRight:"2vw"}}>Log in</button> <button className="w3-btn w3-blue w3-card-4 w3-round-large" onClick={() => navigate('/Register')}>Register</button>
                 </div>
             </div>
         </div>}
-    );
+    </>);
 }
+
+export default LoginPage;
--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -1,17 +1,49 @@
+import axios from "axios";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    username:'',
-    password:'',
-    validPwd:false,
-    validUsername:false,
+    userData:{
+        id: "",
+        password: "",
+        name: "",
+        surname: "",
+        email: "",
+        phone: "",
+        list: []
+      },
+    validPwd:true,
+    validUsername:true,
+
 }
 
 const loginSlice = createSlice({
     name:'login',
     initialState,
+    verifyClient:(state,{payload})=>{
+        axios.get("http://localhost:4000/accounts/"+payload.)
+            .then(function (result) {
+            if(result.data.password===){
+                state.validPwd=true;
+                state.userData.id=result.data.id;
+                state.userData.password=result.data.password;
+                state.userData.name=result.data.name;
+                state.userData.surname=result.data.surname;
+                state.userData.email=result.data.email;
+                state.userData.phone=result.data.phone;
+                state.userData.list=result.data.list;
+            }
+            else{
+                state.validPwd=false;
+            }
+        })
+          .catch(function (error) {
+            console.log(error);
+            state.validUsername=false;
+          });
+    },
 });
 
 //console.log(loginSlice);
 
+export const {verifyClient} = loginSlice.actions;
 export default loginSlice.reducer;
